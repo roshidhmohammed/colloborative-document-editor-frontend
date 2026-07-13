6ddc74493b0ec3b6323e4ee5978aff8ffe15065b
roshidhmohammed <roshidhmohammed777@gmail.com>
2026-07-14 00:18:00 +0530

    debugging env variables issue

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,3 +3,10 @@ export const env = {
   APP_ENV: process.env.NEXT_PUBLIC_APP_ENV!,
   SOCKET_URL:process.env.NEXT_PUBLIC_SOCKET_URL || "https://colloborative-doc-editor-backend-production.up.railway.app/"
 };
+
+
+console.log("===== ENV DEBUG =====");
+console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
+console.log("APP_ENV:", process.env.NEXT_PUBLIC_APP_ENV);
+console.log("SOCKET_URL:", process.env.NEXT_PUBLIC_SOCKET_URL);
+console.log("=====================");
